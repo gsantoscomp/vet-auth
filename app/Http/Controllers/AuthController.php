2515fb77b68a87748015a10a21a4b5e3f4f392f1
d2p-guilherme <gsantos.comp@gmail.com>
2023-07-21 12:45:32 -0300

    feat: ACL - Backend

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Gsantoscomp\SharedVetDb\Models\Permission;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class AuthController extends Controller
 {
@@ -21,6 +23,18 @@ class AuthController extends Controller
         if (!$token = auth()->attempt($credentials)) {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
+
+        $authenticatedUser = auth()->user();
+        $userPermissions = Permission::select('name')
+                            ->where('user_type_id', $authenticatedUser->user_type_id)
+                            ->get()
+                            ->pluck('name');
+        
+        Cache::put(
+            'permissions_' . $authenticatedUser->id,
+            $userPermissions,
+            auth()->factory()->getTTL() * 60
+        );
 
         return $this->respondWithToken($token);
     }
@@ -44,11 +58,18 @@ class AuthController extends Controller
 
     protected function respondWithToken($token)
     {
+        $authenticatedUser = auth()->user();
+        $permissions = Permission::select('name')
+                        ->where('user_type_id', $authenticatedUser->user_type_id)
+                        ->get()
+                        ->pluck('name');
+
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
             'expires_in' => auth()->factory()->getTTL() * 60,
-            'user' => auth()->user()
+            'user' => $authenticatedUser,
+            'permissions' => $permissions
         ]);
     }
 }
---
+++ app/Http/Middleware/CheckPermissions.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckPermissions
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $authenticatedUser = auth()->user();
+        $permissionsInCache = Cache::get('permissions_'. $authenticatedUser->id);
+
+        $path = $request->path();
+
+        if (!$permissionsInCache || !in_array($path, $permissionsInCache)) {
+            return response()->json(['error' => 'Permission denied.'], 403);
+        }
+
+        return $next($request);
+    }
+}
