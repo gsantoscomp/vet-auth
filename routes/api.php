266ed3cad23f045f54f2f2b93c64ee7101141182
d2p-guilherme <gsantos.comp@gmail.com>
2023-07-21 00:06:28 -0300

    feat: adição de dados de usuario na rota de login

--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,5 @@ Route::group(['prefix' => 'auth', 'middleware' => 'api'], function ($router) {
     Route::post('login', [AuthController::class, 'login']);
     Route::post('logout', [AuthController::class, 'logout']);
     Route::post('refresh', [AuthController::class, 'refresh']);
-    Route::post('me', [AuthController::class, 'me']);
+    Route::get('me', [AuthController::class, 'me']);
 });
